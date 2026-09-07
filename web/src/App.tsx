d6afa2df1fb7a86bae56bf9dd24fec40eccc420d
Pk-Dev-ui <pk197782@outlook.com>
2026-09-07 12:39:49 -0700

    Replace authentication templates with TestLogin component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,6 @@
-import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
-import Login from "./components/Login";
-import TaskDashboard from "./components/TaskDashboard";
+import TestLogin from "./TestLogin";
 
 export default function App() {
-  return (
-    <>
-      <UnauthenticatedTemplate>
-        <Login />
-      </UnauthenticatedTemplate>
-      <AuthenticatedTemplate>
-        <TaskDashboard />
-      </AuthenticatedTemplate>
-    </>
-  );
+  return <TestLogin />;
 }
+
